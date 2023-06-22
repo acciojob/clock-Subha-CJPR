41d3f0161d1b568a98a513ce58bb5df7a55b5c5f
Subha-CJPR <56904625+Subha-CJPR@users.noreply.github.com>
2023-06-22 22:02:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateTimer() {
+      var timerElement = document.getElementById('timer');
+      var now = new Date();
+      
+      var date = now.toDateString();
+      var time = now.toLocaleTimeString();
+
+      timerElement.textContent = 'Date: ' + date + ' | Time: ' + time;
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
